a367fc63323fa66b0c2179766f0e1db348e52d5b
EstherOVO <tmejwjd@gmail.com>
2024-04-04 08:47:54 +0900

    MySQL Alter Table

--- Query/Alter Table.sql
+++ Query/Alter Table.sql
@@ -33,3 +33,61 @@ RENAME TO employees_backup;
 -- 원상복구
 ALTER TABLE employees_backup
 RENAME TO employees;
+
+DESCRIBE employees;
+
+-- ---------------------------------------
+
+-- 제약조건 추가하기
+ALTER TABLE employees ADD COLUMN email VARCHAR(255);
+
+-- UNIQUE 제약조건 추가
+ALTER TABLE employees ADD UNIQUE (email);
+
+-- 1. 외래 키 제약조건 추가
+-- 1) 참조할 테이블 생성
+CREATE TABLE departments (
+	department_ID INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+
+-- 2) 외래 키로 사용할 필드 추가
+ALTER TABLE employees ADD COLUMN department_ID INT;
+
+-- 3) 외래 키 제약조건 추가
+ALTER TABLE employees ADD CONSTRAINT fk_department_ID
+FOREIGN KEY (department_ID) REFERENCES departments (department_ID);
+
+DESCRIBE employees;
+
+-- 2. 체크 제약조건 추가
+-- 1) 체크할 필드 추가
+ALTER TABLE employees ADD COLUMN age INT;
+
+-- 2) 체크 제약조건 추가(제약조건의 이름을 명시 : CONSTRAINT 제약조건이름)
+ALTER TABLE employees ADD CONSTRAINT chk_age
+CHECK (age >= 18);
+
+-- 열에 대한 설명 추가
+ALTER TABLE employees ADD COLUMN salary INT COMMENT '직원의 급여와 관련된 속성';
+
+-- 3. 제약조건 삭제하기
+-- 1) 외래 키 삭제하기
+ALTER TABLE employees DROP FOREIGN KEY fk_department_ID;
+
+-- 2) 체크 명시 제약조건 삭제하기
+ALTER TABLE employees DROP CHECK chk_age;
+
+-- ---------------------------------------
+
+-- ALTER COLUMN으로 열 속성 변경하기
+-- 1. 기본 값 추가
+ALTER TABLE employees ALTER COLUMN age SET DEFAULT 20;
+
+-- 2. 기본 값 삭제
+ALTER TABLE employees ALTER COLUMN age DROP DEFAULT;
+
+DESCRIBE departments;
+DESCRIBE employees;
+SELECT * FROM departments;
+SELECT * FROM employees;
